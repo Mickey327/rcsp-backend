22da23b4ae194839c55de8ff4880d2c09c8045db
mickey327 <usp2002@mail.ru>
2023-05-24 01:01:08 +0300

    little fix

--- internal/app/user/dto.go
+++ internal/app/user/dto.go
@@ -3,7 +3,7 @@ package user
 type DTO struct {
 	ID       uint64 `json:"id,omitempty" query:"id"`
 	Email    string `json:"email,omitempty" query:"email" validate:"required,email"`
-	Password string `json:"password,omitempty" query:"password" validate:"required,min=8"`
+	Password string `json:"password,omitempty" query:"password" validate:"required,min=5"`
 	Role     string `json:"role,omitempty" query:"role"`
 }
 
